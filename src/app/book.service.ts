e9baa418273bbbd8ee6b7754fd30802ed5b6ba9c
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-04-15 09:25:23 +0700

    them bookService

---
+++ src/app/book.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
+import {Observable} from 'rxjs';
+import {IBook} from './ibook';
+const URL_BACKEND = environment.api_url;
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  constructor(private httpClient: HttpClient) { }
+  getAllBooks(): Observable<IBook>[] {
+    // @ts-ignore
+    return this.httpClient.get<IBook[]>(URL_BACKEND);
+  }
+  getBookById(id: number): Observable<IBook>{
+    return this.httpClient.get<IBook>(URL_BACKEND + id);
+  }
+  createNewBook(book: IBook): Observable<any>{
+    return this.httpClient.post(URL_BACKEND, book);
+  }
+  editBook(id: number, book: IBook): Observable<any>{
+    return this.httpClient.put(URL_BACKEND + id, book);
+  }
+  deleteBook(id: number): Observable<any>{
+    return this.httpClient.delete(URL_BACKEND + id);
+  }
+}
