a3c30c182f6fb323e2d2a1b230320c4b1370eea7
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-04-15 09:36:43 +0700

    tao routing

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BookRoutingModule } from './book-routing.module';
+import { ListComponent } from './list/list.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { DeleteComponent } from './delete/delete.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListComponent,
+    CreateComponent,
+    EditComponent,
+    DeleteComponent
+  ],
   imports: [
     CommonModule,
     BookRoutingModule
