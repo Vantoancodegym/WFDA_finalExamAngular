f95e15ad2a1e9f58b4ae965de7e5653cd8ffcee8
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-04-15 10:52:35 +0700

    done

--- src/app/book/detail/detail.component.ts
+++ src/app/book/detail/detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Subscription} from 'rxjs';
+import {IBook} from '../../ibook';
+import {BookService} from '../../book.service';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 
 @Component({
   selector: 'app-detail',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
+// @ts-ignore
+  sub: Subscription;
+  book: IBook ={
+    id: 0,
+    title: '',
+    description: '',
+    author: ''
+  }
+  id: number = 0;
 
-  constructor() { }
+  constructor(private bookService: BookService, private router: Router, private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe((pramMap: ParamMap) =>{
+      // @ts-ignore
+      this.id = +pramMap.get("id");
+      this.getBookById(this.id);
+    })
+  }
 
   ngOnInit(): void {
+  }
+  getBookById(id: number){
+    this.bookService.getBookById(id).subscribe(book =>{
+      this.book = book;
+    })
   }
 
 }
